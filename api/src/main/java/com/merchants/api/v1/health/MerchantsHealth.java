89155a2c7792ddbea379181382dfc3a330e17004
barbara7777 <badobe.lipnik@gmail.com>
2023-01-12 02:28:48 +0100

    changet health checks, limited connection pool size

--- api/src/main/java/com/merchants/api/v1/health/MerchantsHealth.java
+++ api/src/main/java/com/merchants/api/v1/health/MerchantsHealth.java
@@ -1,27 +1,27 @@
-//package com.merchants.api.v1.health;
-//
-//
-//import com.merchants.services.config.RestProperties;
-//import org.eclipse.microprofile.health.HealthCheck;
-//import org.eclipse.microprofile.health.HealthCheckResponse;
-//import org.eclipse.microprofile.health.Liveness;
-//
-//import javax.enterprise.context.ApplicationScoped;
-//import javax.inject.Inject;
-//
-//@Liveness
-//@ApplicationScoped
-//public class MerchantsHealth implements HealthCheck {
-//
-//    @Inject
-//    private RestProperties restProperties;
-//
-//    @Override
-//    public HealthCheckResponse call() {
-//        if (restProperties.getBroken()) {
-//            return HealthCheckResponse.down(MerchantsHealth.class.getSimpleName());
-//        } else {
-//            return HealthCheckResponse.up(MerchantsHealth.class.getSimpleName());
-//        }
-//    }
-//}
+package com.merchants.api.v1.health;
+
+
+import com.merchants.services.config.RestProperties;
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@Liveness
+@ApplicationScoped
+public class MerchantsHealth implements HealthCheck {
+
+    @Inject
+    private RestProperties restProperties;
+
+    @Override
+    public HealthCheckResponse call() {
+        if (restProperties.getBroken()) {
+            return HealthCheckResponse.down(MerchantsHealth.class.getSimpleName());
+        } else {
+            return HealthCheckResponse.up(MerchantsHealth.class.getSimpleName());
+        }
+    }
+}
